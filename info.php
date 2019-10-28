c16afcafaaf7e9edc2bd9a9d6c51b470af4fbccd
Gilad Gochman <ggochman@gmail.com>
2019-10-29 00:03:59 +0200

    Add files via upload

--- info.php
+++ info.php
@@ -21,6 +21,20 @@ for ($i=1; $i < 10; $i++) {
     if ($_GET[tristate."$i$j"]==""){
     echo '<input class=sqare2 type=number max=9 min=1 name='.$i.$j.'>';
     }
+	elseif($_GET[tristate."$i$j"]=="╲")
+	{    
+        ##echo '<input class=known readonly=true name=test value='.$_GET[tristate."$i$j"].'>';
+	echo 
+'<div class=known> 
+
+<input class=right type=number name=test >
+
+<input class=down type=number name=test2 >
+</div>';
+
+    }
+
+	
     else{    
         echo '<input class=known readonly=true name='.$i.$j.' value='.$_GET[tristate."$i$j"].'>';
     }
