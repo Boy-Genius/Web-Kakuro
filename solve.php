2533c2cbcb3e81ecb546600860d95e5216467941
Gilad Gochman <ggochman@gmail.com>
2019-10-30 08:02:44 +0200

    Update solve.php

--- solve.php
+++ solve.php
@@ -1,1 +1,11 @@
-<?php ?>
+<?php 
+$i = 11;
+while ($i<=99){
+  ##there might be a reason to check if i%10=0
+  if ($_GET [$i]==""){
+  $"array$i" = (1,2,3,4,5,6,7,8,9);
+   }
+  ##elseif ($_GET [$i]>0  && $_GET [$i]<10){ }
+}
+
+?>
